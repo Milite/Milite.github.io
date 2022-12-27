55c25d4500b45b703163a7620a2d290a5bcd85d4
Milite <m.2001.militello@gmail.com>
2022-12-27 10:09:52 +0100

    -a

--- Appunti/Astrofisica/compile.bat
+++ Appunti/Astrofisica/compile.bat
@@ -2,6 +2,8 @@ pandoc --verbose ^
     -H style\preambolo.tex ^
     --number-sections ^
     style\settings.md ^
+    capitoli\introduzione.md ^
+    capitoli\struttura_stellare.md ^
     capitoli\evoluzione_stellare.md ^
     capitoli\galassie.md ^
     -o out\astrofisica.pdf
