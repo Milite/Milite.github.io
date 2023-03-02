be878ca0095eaffbc3e0401356a2ce9db5002780
milite <m.2001.militello@gmail.com>
2023-03-02 09:51:50 +0100

    -a

---
+++ Appunti/storia_della_scienza/compile.bat
@@ -0,0 +1,7 @@
+pandoc --verbose ^
+    -H style\preambolo.tex ^
+    --number-sections ^
+    style\settings.md ^
+    Capitoli\2023-03-01.md ^
+    Capitoli\2023-03-02.md ^
+    -o out\storia_della_scienza.pdf
